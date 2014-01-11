ee651feeaabb66d6484b606cc42d909e9c902cc0
oferpaster <oferpaster@gmail.com>
2014-01-11 13:40:10 +0200

    -add DailyStatistic controller -add daily statistic to statistic panel -code upstream to the jtable in statistic panel -add TableColumnAdjuster for adjust the jtable view

--- vcproject/src/controler/DailyStatistic.java
+++ vcproject/src/controler/DailyStatistic.java
@@ -1,5 +1,6 @@
 package controler;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -19,19 +20,39 @@ public class DailyStatistic extends Controller {
 		super(host);
 	}
 	
-	/*public Object[] getDailyStatistic(Date date){
-		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-		Object[] daily = {"SELECT * FROM vcp_employ.daily_statistic WHERE date LIKE ?;", format.format(date)};
-		ArrayList<Object> result = getResult();
-		if(!result.get(0).equals("No Result")){
-			Vector<String> cols;
-			String[] columnNames ={"Change number", "Employee ID", "Occasional", "One-Time", "Date", "Status"}; 
-			int columnLength = columnNames.length;
-			cols = new Vector<String>(columnLength);
-			for (int i = 0; i < columnLength; i++)
-				cols.add(columnNames[i]);
+	public Vector<Vector<Object>> getDailyStatistic(Date date){
+		Vector<Vector<Object>> data = new Vector<Vector<Object>>();
+		Integer columnLength = 7;
+		try {
+			SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+			Object[] daily = {"SELECT * FROM vcp_employ.daily_statistic WHERE date = ?'%';", format.format(date)};
+			sendQueryToServer(daily);
+			ArrayList<Object> result = getResult();
+			if(!result.get(0).equals("No Result")){
+				Vector<Object> row = new Vector<Object>(columnLength);
+				for (Object value : result) {
+					if (!value.equals("no value")){
+						if(row.size() == 6){
+							DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+							String dateAndTime = dateFormat.format(value);
+							row.add(dateAndTime);
+						}else
+							row.add(value);
+					}
+					else
+						row.add("0");
+					if(row.size() == 7){
+						data.add(row);
+						row = new Vector<Object>(columnLength);
+					}
+				}
+			}else
+				throw new Exception("There no statistic in DB");
+			return data;
+		} catch (Exception e) {
+			showWarningMsg(e.getMessage());
 		}
-		
-	}*/
+		return null;
+	}
 
 }
--- vcproject/src/gui/Statistics.java
+++ vcproject/src/gui/Statistics.java
@@ -1,16 +1,20 @@
 package gui;
 
-import java.awt.SystemColor;
-
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableModel;
-
+import javax.swing.table.TableCellRenderer;
+import controler.DailyStatistic;
 import controler.WeeklyStats;
-
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Date;
+import java.util.Vector;
+
+import com.toedter.calendar.JDateChooser;
+
+import javax.swing.border.TitledBorder;
 
 public class Statistics extends JPanel {
 	/**
@@ -25,6 +29,8 @@ public class Statistics extends JPanel {
 	private int port = 5555;
 	private JButton btnDailyStatistics;
 	private JScrollPane scrollPane;
+	private JDateChooser dateChooser;
+	private DailyStatistic dailyStatistic;
 
 	public Statistics(String host, int dEFAULT_PORT) {
 		super();
@@ -38,44 +44,52 @@ public class Statistics extends JPanel {
 	private void initialize() {
 		setLayout(null);
 		this.setSize(785, 575);
-		 setBackground(SystemColor.activeCaption);
 		 
 		 btnReturn = new JButton("Return");
 		 btnReturn.setBounds(10, 519, 93, 35);
 		 add(btnReturn);
 		 
 		 JLabel lblStatistics = new JLabel("Statistics");
-		 lblStatistics.setFont(new Font("Tahoma", Font.BOLD, 18));
-		 lblStatistics.setBounds(331, 11, 123, 28);
+		 lblStatistics.setFont(new Font("Tahoma", Font.BOLD, 24));
+		 lblStatistics.setBounds(337, 0, 110, 29);
 		 add(lblStatistics);
-		 
-		 scrollPane = new JScrollPane();
-		 scrollPane.setBounds(10, 107, 765, 400);
-		 add(scrollPane);
 		 
 		 table=new JTable(){
 			 /**
 			 * 
 			 */
 			private static final long serialVersionUID = 1L;
-
 			public boolean isCellEditable(int data,int columnNames){
 				 return false;
 			 }
-				
-			
+			public boolean getScrollableTracksViewportWidth() {
+				   return getPreferredSize().width < getParent().getWidth();
+				 }
 		 };
-		 scrollPane.setViewportView(table);
+		 scrollPane = new JScrollPane(table);
+		 scrollPane.setBounds(10, 175, 765, 332);
+		 add(scrollPane);
 		 
 		 btnWeeklyStatistics = new JButton("Weekly Statistics");
 		 btnWeeklyStatistics.setFont(new Font("Tahoma", Font.BOLD, 12));
-		 btnWeeklyStatistics.setBounds(157, 68, 137, 28);
+		 btnWeeklyStatistics.setBounds(445, 70, 137, 28);
 		 add(btnWeeklyStatistics);
+		 
+		 JPanel panel = new JPanel();
+		 panel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Daily Statistics", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		 panel.setBounds(182, 70, 149, 84);
+		 add(panel);
+		 panel.setLayout(null);
 
 		 btnDailyStatistics = new JButton("Daily Statistics");
+		 btnDailyStatistics.setEnabled(false);
+		 btnDailyStatistics.setBounds(6, 16, 137, 28);
+		 panel.add(btnDailyStatistics);
 		 btnDailyStatistics.setFont(new Font("Tahoma", Font.BOLD, 12));
-		 btnDailyStatistics.setBounds(10, 68, 137, 28);
-		 add(btnDailyStatistics);
+		 
+		 dateChooser = new JDateChooser();
+		 dateChooser.setBounds(6, 57, 137, 20);
+		 panel.add(dateChooser);
 	}
 	
 	private void listners(){
@@ -84,9 +98,50 @@ public class Statistics extends JPanel {
 		 	public void actionPerformed(ActionEvent arg0) {
 		 		getWeeklyStats();
 		 		table.setModel(new DefaultTableModel(weekController.toVector(),weekController.obtainFields()));
+		 		arrangeTable();
 		 	}
 		 });
 		
+		dateChooser.getCalendarButton().addActionListener(new ActionListener() {
+		 	public void actionPerformed(ActionEvent arg0) {
+		 		btnDailyStatistics.setEnabled(true);
+		 	}
+		 });
+		
+		btnDailyStatistics.addActionListener(new ActionListener() {
+		 	public void actionPerformed(ActionEvent e) {
+		 		String[] columnNames ={"Statistic num", "Implement Order", "Canceled Orders", "No. of members", "Members with more then one car", "Late","Date"}; 
+				int columnLength = columnNames.length;
+				Vector<String> cols = new Vector<String>(columnLength);
+				for (int i = 0; i < columnLength; i++)
+					cols.add(columnNames[i]);
+		 		Date date = dateChooser.getDate();
+		 		Vector<Vector<Object>> data = getDailyStatistic().getDailyStatistic(date);
+		 		if(data != null){
+		 			table.setModel(new DefaultTableModel(data,cols));
+		 			arrangeTable();
+		 		}
+		 	}
+		 });
+		
+	}
+	
+	public void arrangeTable(){
+			DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+ 	        centerRenderer.setHorizontalAlignment( JLabel.CENTER );
+ 	        table.setDefaultRenderer(String.class, centerRenderer);
+ 	        for(int i=0;i<table.getColumnCount();i++){
+ 	        	table.getColumnModel().getColumn(i).setCellRenderer( centerRenderer );
+ 	        }
+			TableCellRenderer rendererHeader = table.getTableHeader().getDefaultRenderer();
+			JLabel label = (JLabel)rendererHeader;
+			label.setHorizontalAlignment(JLabel.CENTER);
+			
+			table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+			TableColumnAdjuster tca = new TableColumnAdjuster(table);
+			tca.adjustColumns();
+			scrollPane.revalidate();
+			scrollPane.repaint();
 	}
 
 	
@@ -104,5 +159,11 @@ public class Statistics extends JPanel {
 		}
 			
 		return weekController;
+	}
+
+	public DailyStatistic getDailyStatistic() {
+		if(dailyStatistic == null)
+			dailyStatistic = new DailyStatistic(host, port);
+		return dailyStatistic;
 	}
 }
